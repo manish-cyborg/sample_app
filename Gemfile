00c6a85311a285d87419692f34e4ffabd2063a4e
Manish Kumar <manish.kumar5@tcs.com>
2014-03-26 10:05:34 +0530

    Added bootstrap

--- Gemfile
+++ Gemfile
@@ -59,13 +59,15 @@ group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
 end
-gem 'capybara', '2.1.0'
+#gem 'capybara', '2.1.0'
 gem 'sass-rails'
 gem 'uglifier'
 gem 'coffee-rails'
 gem 'jquery-rails'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets'
 
 # group :doc do
 #   gem 'sdoc', '0.3.20', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,17 @@ SampleApp::Application.routes.draw do
   #get "static_pages/home"
 
   #get "static_pages/help"
-  root :to => 'static_pages#home'
+  # root :to => 'static_pages#home'
+  # match '/help', to: help_path, via: 'get'
+  # match '/about', to: about_path, via: 'get'
+  # match '/contact', to: contact_path, via: 'get'
+  # match '/home', to: home_path, via: 'get'
 
-  match '/static_pages/home', :to => 'static_pages#home'
-  match '/static_pages/help', :to => 'static_pages#help'
-  match '/static_pages/about', :to => 'static_pages#about'
-  match '/static_pages/contact', :to => 'static_pages#contact'
 
+  root  'static_pages#home'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
